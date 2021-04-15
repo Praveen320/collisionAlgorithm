fafe8796650b60a7de10528326af0f045f1df4f4
ksparimala <72539701+ksparimala@users.noreply.github.com>
2021-04-15 12:41:43 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,43 +1,78 @@
-var fixedRect, movingRect,car,wall;
+var fixedRect, movingRect;
+var car,wall;
 
 function setup() {
   createCanvas(1200,800);
   fixedRect = createSprite(600, 400, 50, 80);
   fixedRect.shapeColor = "green";
   fixedRect.debug = true;
+
   movingRect = createSprite(400,200,80,30);
   movingRect.shapeColor = "green";
   movingRect.debug = true;
 
-  wall=createSprite(1000,150,60,90);
-  wall.shapeColor="pink";
-  
-  
-  car=createSprite(100,150,90,40)
-  car.shapeColor="blue";
-  car.velocityX=4;
+  car=createSprite(200,200,50,50)
+  car.shapeColor = "pink";
+  car.velocityX=3;
+
+  wall=createSprite(1000,200,50,90)
+  wall.shapeColor = "pink";
 }
 
 function draw() {
   background(0,0,0);  
   movingRect.x = World.mouseX;
   movingRect.y = World.mouseY;
-  //calling the function
-  if(touching(car,wall))
-  {
-    car.velocityX=0;
-  }
   
-  if(touching(movingRect,fixedRect))
-  {
-    movingRect.shapeColor = "red";
-    fixedRect.shapeColor = "red";
+  //isTouching for movingRect and fixedrect
+  if(isTouching(movingRect,fixedRect)){
+    movingRect.shapeColor = "orange";
+    fixedRect.shapeColor = "orange";
   }
-  else {
+  else{
     movingRect.shapeColor = "green";
     fixedRect.shapeColor = "green";
   }
 
-   drawSprites();
+  //isTouching for car and wall
+  if(isTouching(car,wall))
+  {
+    car.shapeColor = "blue";
+    wall.shapeColor = "blue";
+    car.velocityX=0;
+  }
+  else{
+    car.shapeColor = "pink";
+    wall.shapeColor = "pink";
+  }
+
+  drawSprites();
 }
+//user defined function  &  object1 and object2 are the function arguments
+function isTouching(object1,object2)
+{
+  if (object1.x - object2.x < object2.width/2 + object1.width/2
+    && object2.x - object1.x < object2.width/2 + object1.width/2
+    && object1.y - object2.y < object2.height/2 + object1.height/2
+    && object2.y - object1.y < object2.height/2 + object1.height/2) {
+  return true;
+}
+else {
+return false;
+}
+}
+// revision
+//1.We created user defined isTouching outside of the draw function
+//2.we need to call the function inside the draw function
+//3.chaning the color to be done when we are calling the function we have return statement in function
+//4.we also created 2 more sprites and made the isTouching function to work all the objects
+
+
+
+
+
+
+
+
+
 
